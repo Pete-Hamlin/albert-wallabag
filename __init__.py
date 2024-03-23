7b2b75aa0f993d4ce580dee48cd521f1ea154317
pete <work@pete-hamlin.com>
2024-03-23 13:38:52 +0000

    fix: Fix indexer, retired cache_length setting

--- __init__.py
+++ __init__.py
@@ -7,9 +7,8 @@ from urllib import parse
 import requests
 from albert import *
 
-
 md_iid = "2.2"
-md_version = "3.0"
+md_version = "3.1"
 md_name = "Wallabag"
 md_description = "Manage saved articles via a wallabag instance"
 md_license = "MIT"
@@ -17,20 +16,19 @@ md_url = "https://github.com/Pete-Hamlin/albert-python"
 md_authors = ["@Pete-Hamlin"]
 md_lib_dependencies = ["requests"]
 
+
 class ArticleFetcherThread(Thread):
-    def __init__(self, callback, cache_length, *args, **kwargs) -> None:
+    def __init__(self, callback, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.__stop_event = Event()
         self.__callback = callback
-        self.__cache_length = cache_length * 60
+        # 15 minutes
+        self.__cache_length = 9000
 
     def run(self):
-        # Watch for file changes and re-index
         while True:
-            self.__callback
             self.__stop_event.wait(self.__cache_length)
-            if self.__stop_event.is_set():
-                return
+            self.__callback()
 
     def stop(self):
         self.__stop_event.set()
@@ -42,7 +40,9 @@ class Plugin(PluginInstance, IndexQueryHandler):
     user_agent = "org.albert.wallabag"
 
     def __init__(self):
-        IndexQueryHandler.__init__(self, id=md_id, name=md_name, description=md_description, synopsis="<article-name>", defaultTrigger="wb ")
+        IndexQueryHandler.__init__(
+            self, id=md_id, name=md_name, description=md_description, synopsis="<article-name>", defaultTrigger="wb "
+        )
         PluginInstance.__init__(self, extensions=[self])
 
         self._instance_url = self.readConfig("instance_url", str) or "http://localhost:80"
@@ -51,16 +51,14 @@ class Plugin(PluginInstance, IndexQueryHandler):
         self._client_id = self.readConfig("client_id", str) or ""
         self._client_secret = self.readConfig("client_secret", str) or ""
 
-        self._cache_length = self.readConfig("cache_length", int) or 15
+        self._token = None
 
-        self.token = None
-
-        self.thread = ArticleFetcherThread(callback=self.updateIndexItems, cache_length=self.cache_length)
-        self.thread.start()
+        self._thread = ArticleFetcherThread(callback=self.updateIndexItems)
+        self._thread.start()
 
     def finalize(self):
-        self.thread.stop()
-        self.thread.join()
+        self._thread.stop()
+        self._thread.join()
 
     @property
     def instance_url(self):
@@ -107,19 +105,6 @@ class Plugin(PluginInstance, IndexQueryHandler):
         self._client_secret = value
         self.writeConfig("client_secret", value)
 
-    @property
-    def cache_length(self):
-        return self._cache_length
-
-    @cache_length.setter
-    def cache_length(self, value):
-        self._cache_length = value
-        self.writeConfig("cache_length", value)
-
-        self.thread.stop()
-        self.thread.join()
-        self.thread = ArticleFetcherThread(target=self.updateIndexItems, cache_length=self.cache_length)
-
     def configWidget(self):
         return [
             {"type": "lineedit", "property": "instance_url", "label": "URL"},
@@ -137,16 +122,15 @@ class Plugin(PluginInstance, IndexQueryHandler):
                 "label": "Client Secret",
                 "widget_properties": {"echoMode": "Password"},
             },
-            {"type": "spinbox", "property": "cache_length", "label": "Cache length (minutes)"},
         ]
 
     def updateIndexItems(self):
         start = perf_counter_ns()
-        data = self.fetch_results()
+        data = self._fetch_results()
         index_items = []
         for article in data:
-            filter = self.create_filters(article)
-            item = self.gen_item(article)
+            filter = self._create_filters(article)
+            item = self._gen_item(article)
             index_items.append(IndexItem(item=item, string=filter))
         self.setIndexItems(index_items)
         info("Indexed {} articles [{:d} ms]".format(len(index_items), (int(perf_counter_ns() - start) // 1000000)))
@@ -171,32 +155,31 @@ class Plugin(PluginInstance, IndexQueryHandler):
     #             )
     #         )
 
-
-    def create_filters(self, item: dict):
+    def _create_filters(self, item: dict):
         return ",".join([item["url"], item["title"].lower(), ",".join(tag["label"] for tag in item["tags"])])
 
-    def gen_item(self, article: object):
-            return StandardItem(
-                id=md_id,
-                text=article["title"] or article["url"],
-                subtext=" - ".join([article["url"], ",".join(tag["label"] for tag in article["tags"])]),
-                iconUrls=self.iconUrls,
-                actions=[
-                    Action(
-                        "open",
-                        "Open article in wallabag",
-                        lambda u="{}/view/{}".format(self._instance_url, article["id"]): openUrl(u),
-                    ),
-                    Action("open-url", "Open article URL", lambda u=article["url"]: openUrl(u)),
-                    Action("copy", "Copy article URL to clipboard", lambda u=article["url"]: setClipboardText(u)),
-                ],
-            )
+    def _gen_item(self, article: object):
+        return StandardItem(
+            id=md_id,
+            text=article["title"] or article["url"],
+            subtext=" - ".join([article["url"], ",".join(tag["label"] for tag in article["tags"])]),
+            iconUrls=self.iconUrls,
+            actions=[
+                Action(
+                    "open",
+                    "Open article in wallabag",
+                    lambda u="{}/view/{}".format(self._instance_url, article["id"]): openUrl(u),
+                ),
+                Action("open-url", "Open article URL", lambda u=article["url"]: openUrl(u)),
+                Action("copy", "Copy article URL to clipboard", lambda u=article["url"]: setClipboardText(u)),
+            ],
+        )
 
-    def fetch_results(self):
-        headers = {"User-Agent": self.user_agent, "Authorization": f"Bearer {self.get_token()}"}
-        return (article for article_list in self.get_articles(headers) for article in article_list)
+    def _fetch_results(self):
+        headers = {"User-Agent": self.user_agent, "Authorization": f"Bearer {self._get_token()}"}
+        return (article for article_list in self._get_articles(headers) for article in article_list)
 
-    def get_articles(self, headers: dict):
+    def _get_articles(self, headers: dict):
         # Set the initial pages so the loop runs at least once
         page, pages = 0, 1
         while page < pages:
@@ -211,12 +194,12 @@ class Plugin(PluginInstance, IndexQueryHandler):
             else:
                 warning(f"Got response {response.status_code} querying {url}")
 
-    def get_token(self):
-        if not self.token or not self.token.is_valid():
-            self.refresh_token()
-        return self.token.access
+    def _get_token(self):
+        if not self._token or not self._token.is_valid():
+            self._refresh_token()
+        return self._token.access
 
-    def refresh_token(self):
+    def _refresh_token(self):
         url = f"{self._instance_url}/oauth/v2/token"
         debug(f"Fetching token from {url}")
         response = requests.post(
@@ -231,7 +214,7 @@ class Plugin(PluginInstance, IndexQueryHandler):
             timeout=5,
         )
         if response.ok:
-            self.token = Token(response.json())
+            self._token = Token(response.json())
         else:
             warning(f"Got response: {response.status_code} {response.content}")
 
